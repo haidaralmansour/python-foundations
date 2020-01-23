8fdea59f310d134722fc37067a2adc7844e229b2
Haidar AlMansour <haidar.almansour@zain.com>
2020-01-23 14:51:11 +0300

    done

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+print("Mad libs where lbis get mad")
+print("start below:")
+print()
+number = input("enter a number from 1 to 12: ")
+noun= input("enter a noun:  ")
+name=input("enter a name:  ")
+sentence=input("enter any sentence:  ")
+verb=input("enter a verb:  ")
+print()
+print("The story goes...")
+print()
+print("It was %s o'clock when I heard a knock at the door" % number)
+print('I opened the door and there was a box full of %s with a note saying "from mr.%s " ' %(noun,name))
+print('just as I closed the door I heard a scream "%s" ' %(sentence))
+print('I frozen in place and all I could do was %s '%(verb))
+
+
+
+
